1e492201b86c890f0db12701fb8d30d07e04f8cb
Vex-I <nawwaf.aydin@gmail.com>
2025-09-22 01:53:43 +0800

    Base Components

---
+++ src/components/AboutHead.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from '../assets/Meme.jpeg';
+
+const AboutHead = () => (
+    <div>
+        <h1>
+            <span className="red text-heading"> {'>'} </span>
+            <span className="yellow text-heading">{"NawwafSudi"}</span>
+            <span className="grey text-heading">{".Greet()"}</span>
+        </h1>
+        <section>
+            <p className="text white">
+                Hello World! My name is Nawwaf Sudi, an Undergraduate student 
+                studying Data Science and Analytics at the National University of Singapore (
+                <a className= "hyper-link red">NUS</a>). Physicist turned programmer, I am passionate about leveraging technology to solve real-world problems. 
+                I have a keen interest in software development, data science, and machine learning.
+            </p>
+            <p className='text white'>
+                    In a more personal note, I'm also intereseted in game development, and have quite a few projects using Unity, 
+                    collaborating with friends to create games. You can check out some of my projects on both my itch.io page
+                    and my blog <a className="hyper-link red">here</a>.
+            </p>
+            <p className='text white'>
+                In my free time, I enjoy gardening and reading books on a variety of topics, from science fiction to philosophy. 
+                You can find my succulent collection, and my currated reading list in <a className="hyper-link red">my blog</a>.
+            </p>
+            <p className='text white'>
+                I'm always eager to connect, so feel free to <a className="hyper-link red">reach out</a>!
+            </p>
+        </section>
+    </div>
+);
+
+
+
+export default AboutHead;
