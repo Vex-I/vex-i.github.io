89d88ceec90404eddbff8b38ff83389036545e4f
Vex-I <nawwaf.aydin@gmail.com>
2025-09-22 03:23:16 +0800

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar';
 import About from './pages/About';
 
 function App() {
   return (
   <Router>
+    <NavBar />
       <Routes>
         <Route path="/" element={<About />} />
       </Routes>
---
+++ src/components/PageList.jsx
@@ -0,0 +1,9 @@
+const PageList = () => (
+    <div>   
+        <p>
+            <span className="red text-heading"> {'>'} </span>
+            <span className="yellow text-heading">{"ls "}</span>
+            <span className="grey text-heading">{"page"}</span>
+        </p>
+    </div>
+);
