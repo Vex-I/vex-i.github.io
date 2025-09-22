101fb78fecb0d54ecce8fe2b4a0b215589175c50
Vex-I <nawwaf.aydin@gmail.com>
2025-09-22 23:49:39 +0800

    delete

--- src/pages/About.jsx
+++
@@ -1,8 +0,0 @@
-import AboutHead from "../components/AboutHead"
-const About = () => (
-    <AboutHead />
-    ///Here lies the Blog Component. IF I HAD ONE!
-    //Also footer
-);
-
-export default About;
--- src/pages/BlogView.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import BlogList from '../components/BlogList';
-//Page for the blog list view.
-const BlogView = () => (
-    <BlogList />
-);
-
-export default BlogView;
