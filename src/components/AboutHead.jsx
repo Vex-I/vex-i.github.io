d1387df3b215b52649a3a304ff6a68ed57d9b7a6
Vex-I <nawwaf.aydin@gmail.com>
2025-09-23 03:57:21 +0800

    updated AboutHead.jsx

--- src/components/AboutHead.jsx
+++ src/components/AboutHead.jsx
@@ -12,20 +12,20 @@ const AboutHead = () => (
             <p className="text white">
                 Hello World! My name is Nawwaf Sudi, an Undergraduate student 
                 studying Data Science and Analytics at the National University of Singapore (
-                <a className= "hyper-link red">NUS</a>). Physicist turned programmer, I am passionate about leveraging technology to solve real-world problems. 
+                <a className= "hyper-link red" href='https://nus.edu.sg/'>NUS</a>). Physicist turned programmer, I am passionate about leveraging technology to solve real-world problems. 
                 I have a keen interest in software development, data science, and machine learning.
             </p>
             <p className='text white'>
                     In a more personal note, I'm also intereseted in game development, and have quite a few projects using Unity, 
                     collaborating with friends to create games. You can check out some of my projects on both my itch.io page
-                    and my blog <a className="hyper-link red">here</a>.
+                    and my blog <a className="hyper-link red" href='https://github.com/Vex-I'>here</a>.
             </p>
             <p className='text white'>
                 In my free time, I enjoy gardening and reading books on a variety of topics, from science fiction to philosophy. 
-                You can find my succulent collection, and my currated reading list in <a className="hyper-link red">my blog</a>.
+                You can find my succulent collection, and my currated reading list in <a className="hyper-link red" href='https://github.com/Vex-I'>my blog</a>.
             </p>
             <p className='text white'>
-                I'm always eager to connect, so feel free to <a className="hyper-link red">reach out</a>!
+                I'm always eager to connect, so feel free to <a className="hyper-link red" href='https://github.com/Vex-I'>reach out</a>!
             </p>
         </section>
     </div>
